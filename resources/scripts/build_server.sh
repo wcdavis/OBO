1dce3eae961130e42854d6c8e3d3a8c8bbe1b930
beneisner <beisner@princeton.edu>
2015-04-22 03:46:03 -0400

    changes to shell scripts

--- resources/scripts/build_server.sh
+++ resources/scripts/build_server.sh
@@ -1,8 +1,4 @@
-cd /home/ec2-user/go/src/github.com/PrincetonOBO/OBOBackend/
-<<<<<<< HEAD
-=======
 export GOPATH=/home/ec2-user/go
 cp -f resources/swagger/indexes/index.html resources/swagger
->>>>>>> 367df3af0897f66c43420cf2c51b8274b02712d3
 go get
 go build 
--- resources/scripts/start_server.sh
+++ resources/scripts/start_server.sh
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
-=======
 stop OBOBackend
->>>>>>> 367df3af0897f66c43420cf2c51b8274b02712d3
 start OBOBackend
